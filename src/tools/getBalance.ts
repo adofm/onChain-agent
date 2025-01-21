3f35e8e7f4023811dd1482263f1c3d3794a24961
adofm <karthik.maniam008@gmail.com>
2025-01-21 09:48:52 +0530

    Added logic for tools which can be used by the ai agent
    -> all the tools which can be accessed by the ai agent
    -> get balance tool which gets the balance of a ethereum sepolia address

---
+++ src/tools/getBalance.ts
@@ -0,0 +1,34 @@
+import { Address } from "viem";
+import { ToolConfig } from "./allTools";
+import { formatEther } from "viem";
+import { createViemPublicClient } from "../viem/createPublicClient";
+
+interface GetBalanceArgs{
+    wallet:Address
+}
+
+export const getBalanceTool : ToolConfig<GetBalanceArgs> = {
+    definition:{
+        type:'function',
+        function:{
+            name:'get_balance',
+            description:'Get the balance of a wallet',
+            parameters:{
+                type:'object',
+                properties:{
+                    wallet:{
+                        type:'string',
+                        pattern:'^0x[a-fA-F0-0]{40}$',
+                        description:'The wallet address to get the balance of ',
+                    }
+                },
+                required:['wallet']
+            }
+        }
+    },
+    handler: async({wallet}) =>{
+        const publicClient=createViemPublicClient();
+        const balance = await publicClient.getBalance({address:wallet});
+        return formatEther(balance);
+    }
+};
